cdc6d7e835a76272b124d8e2e622f07958c3b3c4
Standa Vin <vinninjaq@gmail.com>
2022-12-22 23:27:33 +0100

    First Day

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck, faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react';
+
+import './App.css';
 
 function App() {
+
+  // Task (To-Do List) State
+  const [toDo, setToDo] = useState([
+    {id: 1, title: 'Task 1', status: false},
+    {id: 2, title: 'Task 2', status: false},
+  ]);
+
+  // Temp State
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateDate] =useState('');
+
+  // Add task
+  const addTask = () => {
+
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+
+  }
+
+  // Mark task as done or complited
+  const markDone = (id) => {
+
+  }
+
+  // Cancel update 
+  const cancelUpdate = () => {
+
+  }
+
+  // Change task for update
+  const changeTask = (e) => {
+
+  }
+
+  // Update task
+  const updateTask = () => {
+    
+  }
+
   return (
-    <div className="App">
-      WebStylePress
+    <div className="container App">
+
+    <br /><br />
+    <h2>To Do List App</h2>
+    <br /><br />
+
     </div>
   );
 }
